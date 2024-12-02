25adad65f72d7fe01f48e09515fa2f92517c93c3
BlooDMV <117545432+BlooDMV@users.noreply.github.com>
2024-12-02 22:44:14 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-function flipCard() {
-    document.querySelector(".card").classList.toggle("flipped");
-}
+// Select the card container
+const cardContainer = document.querySelector('.card-container');
+
+// Add a click event to toggle the "clicked" class
+cardContainer.addEventListener('click', () => {
+  cardContainer.classList.toggle('clicked');
+});
